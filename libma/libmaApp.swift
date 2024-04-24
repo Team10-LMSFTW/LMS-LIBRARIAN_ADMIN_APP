6e766f9660b11beb589b3a76719def2892dc0b04
mkswagger <mathangykrishna16@gmail.com>
2024-04-24 16:21:48 +0530

    Initial Commit

---
+++ libma/libmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  libmaApp.swift
+//  libma
+//
+//  Created by mathangy on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct libmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
