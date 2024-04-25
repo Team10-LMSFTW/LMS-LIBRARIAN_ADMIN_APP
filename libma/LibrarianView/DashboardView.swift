5107cac2752c397c63e21d71fa6bdbcd699a4fc0
Yuvraj Pandey <yuvraj@Vikashini-MacBook-Pro.local>
2024-04-25 14:11:39 +0530

    added dashboard view and tab bar

---
+++ libma/LibrarianView/DashboardView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        ZStack {
+            Color(hex: 0xFFFFFF)
+                .edgesIgnoringSafeArea(.all)
+            
+            
+        }
+    }
+}
+
+#Preview {
+    DashboardView()
+}
