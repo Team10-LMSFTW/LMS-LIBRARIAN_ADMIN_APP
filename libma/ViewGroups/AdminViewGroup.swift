97e25b4ec9c610f1656ab8390416385d37f14775
utkarsh575 <utkarshlohani575@gmail.com>
2024-04-25 22:04:22 +0530

    :pray: chal ja bhai pls

---
+++ libma/ViewGroups/AdminViewGroup.swift
@@ -0,0 +1,19 @@
+//
+//  AdminViewGroup.swift
+//  libma
+//
+//  Created by admin on 25/04/24.
+//
+
+import SwiftUI
+
+struct AdminViewGroup: View {
+    var body: some View {
+            Text("Hello, Admin!")
+        
+    }
+}
+
+#Preview {
+    AdminViewGroup()
+}
