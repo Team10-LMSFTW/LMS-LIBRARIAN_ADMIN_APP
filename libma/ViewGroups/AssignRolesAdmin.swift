8806a846146cada7e2a5cc2e1fc7b16f0ec2cd2f
Yuvraj Pandey <yuvipandey0011@gmail.com>
2024-04-26 17:52:42 +0530

    assign admin role page fixed

---
+++ libma/ViewGroups/AssignRolesAdmin.swift
@@ -0,0 +1,18 @@
+//
+//  AssignRolesAdmin.swift
+//  libma
+//
+//  Created by Yuvraj Pandey on 26/04/24.
+//
+
+import SwiftUI
+
+struct AssignRolesAdmin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssignRolesAdmin()
+}
