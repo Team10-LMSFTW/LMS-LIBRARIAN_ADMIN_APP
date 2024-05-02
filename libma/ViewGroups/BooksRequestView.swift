91637ddffc69d225d83d6719110e321d71428162
kg4687 <kg4687@srmist.edu.in>
2024-05-02 11:53:20 +0530

    Book request for lending Added

---
+++ libma/ViewGroups/BooksRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  BooksRequestView.swift
+//  libma
+//
+//  Created by admin on 02/05/24.
+//
+
+import SwiftUI
+
+struct BooksRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BooksRequestView()
+}
