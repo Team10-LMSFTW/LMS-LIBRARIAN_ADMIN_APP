44cad9c0afe32c021fc9d511790bcaa3719e3946
kg4687 <kg4687@srmist.edu.in>
2024-05-02 13:58:26 +0530

    BooksRenewableView added

---
+++ libma/ViewGroups/BooksRenewableView.swift
@@ -0,0 +1,18 @@
+//
+//  BooksRenewableView.swift
+//  libma
+//
+//  Created by admin on 02/05/24.
+//
+
+import SwiftUI
+
+struct BooksRenewableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BooksRenewableView()
+}
